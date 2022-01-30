5367ff53f13f53fcbb82337416f9ccbe02682c9b
Antonio Amaral <antonio.amaral@monitoratec.com.br>
2022-01-30 19:32:35 -0300

    Trocando o CrudRepository por JpaRepository

--- produto-ms/src/main/java/com/ifsuldeminas/produtoms/repository/ProdutoRepository.java
+++ produto-ms/src/main/java/com/ifsuldeminas/produtoms/repository/ProdutoRepository.java
@@ -1,8 +1,10 @@
 package com.ifsuldeminas.produtoms.repository;
 
 import com.ifsuldeminas.produtoms.model.ProdutoDTO;
+import com.ifsuldeminas.produtoms.model.TicketDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProdutoRepository extends CrudRepository<ProdutoDTO, Integer> {
+public interface ProdutoRepository extends JpaRepository<ProdutoDTO, Integer> {
     ProdutoDTO findByProdutoId(Integer produtoId);
 }
