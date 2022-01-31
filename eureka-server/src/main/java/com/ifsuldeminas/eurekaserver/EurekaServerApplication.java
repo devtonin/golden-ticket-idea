6ba8757ae8f5b1466c3d38c8a800689e7e761e80
Antonio Amaral <antonio.amaral@monitoratec.com.br>
2022-01-31 17:08:28 -0300

    idéias de to-do para esse projeto

--- eureka-server/src/main/java/com/ifsuldeminas/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ifsuldeminas/eurekaserver/EurekaServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+	// TODO: 31/01/2022 containerizar cada ms através do k8s - replicar o projeto com comunicação via kafka - implementar uma api gateway 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
